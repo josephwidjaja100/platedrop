b8d2bc3c6d87a00f694dd17d8454b2b92117917e
Joseph Widjaja <josephwidjaja@Josephs-MacBook-Pro.local>
2025-12-21 16:16:50 -0500

    web analytics integration

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,8 @@
 
 import React from 'react';
 import { SessionProvider } from 'next-auth/react';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider>{children}<Analytics /></SessionProvider>;
 }
